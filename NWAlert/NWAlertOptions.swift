f15c4b04bd7e72729522351ab53865016a6590ca
Nguyen Kien Trung <trungnk@smartmove.com.vn>
2023-07-04 16:11:43 +0700

    update readme & update options

--- NWAlert/NWAlertOptions.swift
+++ NWAlert/NWAlertOptions.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-public struct NWAlertOptions {
+public class NWAlertOptions {
     public var fontTitle : UIFont = UIFont.boldSystemFont(ofSize: 18)
     public var fontMessage : UIFont = UIFont.systemFont(ofSize: 12)
     public var fontButton : UIFont = UIFont.boldSystemFont(ofSize: 15)
@@ -23,4 +23,30 @@ public struct NWAlertOptions {
     public var widthOfAlert : CGFloat = 300
     public var cornerViewContent : CGFloat = 12
     public var cornerButton : CGFloat = 8
+    
+    public init(fontTitle: UIFont = UIFont.boldSystemFont(ofSize: 18),
+                fontMessage: UIFont = UIFont.systemFont(ofSize: 12),
+                fontButton: UIFont = UIFont.boldSystemFont(ofSize: 15),
+                colorButtonDefault: UIColor = UIColor(red: 87/255, green: 93/255, blue: 255/255, alpha: 1),
+                colorButtonDestructive: UIColor = UIColor(red: 245/255, green: 87/255, blue: 87/255, alpha: 1.0),
+                colorButtonTitleActive: UIColor = .white,
+                backgroundContentColor: UIColor = .white,
+                spacing: CGFloat = 16,
+                heightOfButton: CGFloat = 50,
+                widthOfAlert: CGFloat = 300,
+                cornerViewContent: CGFloat = 12,
+                cornerButton: CGFloat = 8){
+        self.fontTitle = fontTitle
+        self.fontMessage = fontMessage
+        self.fontButton = fontButton
+        self.colorButtonDefault = colorButtonDefault
+        self.colorButtonDestructive = colorButtonDestructive
+        self.colorButtonTitleActive = colorButtonTitleActive
+        self.backgroundContentColor = backgroundContentColor
+        self.spacing = spacing
+        self.heightOfButton = heightOfButton
+        self.widthOfAlert = widthOfAlert
+        self.cornerViewContent = cornerViewContent
+        self.cornerButton = cornerButton
+    }
 }
--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # NWAlert
 
-A description of this package.
+A Sample Popup 
+
+# Requiment
+
+- Swift : 5 
+- Xcode 14
+- iOS 13 trở lên 
+
+# Installation 
+
+**SPM
+
+```
+dependencies: [
+    .package(url: "https://github.com/volythat/NWAlert.git", .upToNextMajor(from: "1.0.0"))
+]
+```  
+
+# Use 
+
+```swift 
+    let alert = NWAlertController(title: "Are you sure?",
+                                  message: "This action can't be undo!",
+                                  buttons: [NWAlertButton("Cancel",style: .destructive),
+                                            NWAlertButton("Discard",style: .destructive,highlight: true)])
+    alert.alertButtonAction = { action in
+        print("action = \(action.title)")
+    }
+    self.present(alert , animated: true)
+
+```
+
+
+# Author : volythat
+
